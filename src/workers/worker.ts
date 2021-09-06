450e75d1701891dcb0bae2f6147c4b75590a1730
Joel <joel@wachsler.se>
2021-09-07 00:15:47 +0200

    Add support for shortened yt urls

--- src/workers/worker.ts
+++ src/workers/worker.ts
@@ -4,13 +4,15 @@ import yts from 'yt-search'
 expose({
   // Using a seperate thread for this because, it's costly for some reason...
   async fetchInfo(query: string) {
-    if (query.startsWith('http')) {
-      const videoIdMatch = query.match(new RegExp(/https:\/\/www.youtube.com\/watch\?.*?v=(\w+).*?/))
-      if (videoIdMatch) {
-        const [_, videoId] = videoIdMatch
-        const res  = await yts.search({ videoId })
-        return JSON.stringify(res)
-      }
+    const normalLink = /(?:https)?:\/\/www.youtube.com\/watch\?.*?v=(\w+).*?/
+    const shortenedLink = /(?:https)?:\/\/youtu.be\/(\w+).*?/
+
+    const videoIdMatch = query.match(normalLink) ?? query.match(shortenedLink)
+
+    if (videoIdMatch) {
+      const [_, videoId] = videoIdMatch
+      const res  = await yts.search({ videoId })
+      return JSON.stringify(res)
     } else {
       const { videos: [ match ] } = await yts.search(query)
       return JSON.stringify(match)
