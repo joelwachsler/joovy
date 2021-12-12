3ce37fa248da74c183c7163304f627637063b44c
Joel <joel@wachsler.se>
2021-12-12 12:09:36 +0000

    Allow user to easier continue after withResult is called

--- src/jevent/impl/Result.ts
+++ src/jevent/impl/Result.ts
@@ -1,15 +1,17 @@
-import { Observable, of } from 'rxjs'
+import { concat, Observable, of } from 'rxjs'
 import JEvent, { BaseConstructor, Result, ResultEntry, ResultResult } from '../JEvent'
 
 const WithResult = <TBase extends BaseConstructor>(Base: TBase) => {
   return class extends Base implements Result {
-    withResult(resultToAdd: ResultResult): Observable<ResultEntry> {
-      return of({
-        result: resultToAdd,
-        // hacky but makes life so much easier by not having the user
-        // to pass event each time this method is called :)
-        event: this as unknown as JEvent,
-      })
+    withResult(resultToAdd: ResultResult, ...andThen: Observable<ResultEntry>[]): Observable<ResultEntry> {
+      return andThen.reduce(
+        (acc, curr) => concat(acc, curr),
+        of({
+          result: resultToAdd,
+          // TODO: make this type safe
+          event: this as unknown as JEvent,
+        }),
+      )
     }
   }
 }
