4ffc78b01cccd809a87dbd589d0856d28037a1ac
Joel <joel@wachsler.se>
2021-12-13 18:22:36 +0000

    Group tests

--- src/messageHandler.spec.ts
+++ src/messageHandler.spec.ts
@@ -60,143 +60,151 @@ const createTestEvent = (input?: Partial<JMessage>): JEvent => {
   }
 }
 
-it('should ignore bot messages', () =>  {
-  const event = createTestEvent({
-    author: {
-      bot: true,
-      id: 'testAuthorId',
-    },
-    content: '/test',
+describe('message filtering', () => {
+  it('should ignore bot messages', () =>  {
+    const event = createTestEvent({
+      author: {
+        bot: true,
+        id: 'testAuthorId',
+      },
+      content: '/test',
+    })
+
+    const messages = handle(hot('a|', { a: event }))
+    expect(messages).toMatchObject(e('a|', {
+      a: {
+        ignored: '/test was sent by a bot',
+      },
+    }))
   })
 
-  const messages = handle(hot('a|', { a: event }))
-  expect(messages).toMatchObject(e('a|', {
-    a: {
-      ignored: '/test was sent by a bot',
-    },
-  }))
+  it('should ignore messages not starting with a slash', () => {
+    const event = createTestEvent({
+      content: 'test',
+    })
+
+    const messages = handle(hot('a|', { a: event }))
+    expect(messages).toMatchObject(e('a|', {
+      a: {
+        ignored: 'test does not start with a slash',
+      },
+    }))
+  })
 })
 
-it('should ignore messages not starting with a slash', () => {
-  const event = createTestEvent({
-    content: 'test',
+describe('player creation', () => {
+  it('should create player if not previously created', () => {
+    const event = createTestEvent({
+      content: '/play test',
+    })
+
+    const messages = handle(hot('a|', { a: event }))
+    expect(messages).toMatchObject(e('(abc)|', {
+      a: {
+        commandCalled: '/play',
+      },
+      b: {
+        player: 'created',
+      },
+      c: {
+        playing: { name: '/play test', link: 'test' },
+      },
+    }))
   })
 
-  const messages = handle(hot('a|', { a: event }))
-  expect(messages).toMatchObject(e('a|', {
-    a: {
-      ignored: 'test does not start with a slash',
-    },
-  }))
+  it('should not create player if previously created', () => {
+    const play = createTestEvent({
+      content: '/play test',
+    })
+
+    const playAgain = createTestEvent({
+      content: '/play test2',
+    })
+
+    const messages = handle(hot('ab|', { a: play, b: playAgain }))
+    expect(messages).toMatchObject(e('(abc)(ade)|', {
+      a: {
+        commandCalled: '/play',
+      },
+      b: {
+        player: 'created',
+      },
+      c: {
+        playing: { name: '/play test', link: 'test' },
+      },
+      d: {
+        player: 'found',
+      },
+      e: {
+        playing: { name: '/play test2', link: 'test2' },
+      },
+    }))
+  })
 })
 
-it('should create player if not previously created', () => {
-  const event = createTestEvent({
-    content: '/play test',
-  })
+describe('misc commands', () => {
+  it('invalid command should call help', () => {
+    const event = createTestEvent({
+      content: '/invalid command',
+    })
 
-  const messages = handle(hot('a|', { a: event }))
-  expect(messages).toMatchObject(e('(abc)|', {
-    a: {
-      commandCalled: '/play',
-    },
-    b: {
-      player: 'created',
-    },
-    c: {
-      playing: { name: '/play test', link: 'test' },
-    },
-  }))
+    const messages = handle(hot('a|', { a: event }))
+    expect(messages).toMatchObject(e('(ab)|', {
+      a: {
+        invalidCommand: '/invalid command',
+      },
+      b: {
+        help: true,
+      },
+    }))
+  })
 })
 
-it('should not create player if previously created', () => {
-  const play = createTestEvent({
-    content: '/play test',
+describe('disconnection', () => {
+  it('disconnect should disconnect if connected to channel', () => {
+    const play = createTestEvent({
+      content: '/play test',
+    })
+    const disconnect = createTestEvent({
+      content: '/disconnect',
+    })
+
+    const messages = handle(hot('ab|', { a: play, b: disconnect }))
+    expect(messages).toMatchObject(e('(abc)(defg)|', {
+      a: {
+        commandCalled: '/play',
+      },
+      b: {
+        player: 'created',
+      },
+      c: {
+        playing: { name: '/play test', link: 'test' },
+      },
+      d: {
+        commandCalled: '/disconnect',
+      },
+      e: {
+        player: 'disconnected',
+      },
+      f: {
+        player: 'removed',
+      },
+      g: {
+        messageSent: 'Bye!',
+      },
+    }))
   })
 
-  const playAgain = createTestEvent({
-    content: '/play test2',
+  test('disconnect should not do anything if not connected to channel', () => {
+    const disconnect = createTestEvent({
+      content: '/disconnect',
+    })
+
+    const messages = handle(hot('a|', { a: disconnect }))
+    expect(messages).toMatchObject(e('a|', {
+      a: {
+        commandCalled: '/disconnect',
+      },
+    }))
   })
-
-  const messages = handle(hot('ab|', { a: play, b: playAgain }))
-  expect(messages).toMatchObject(e('(abc)(ade)|', {
-    a: {
-      commandCalled: '/play',
-    },
-    b: {
-      player: 'created',
-    },
-    c: {
-      playing: { name: '/play test', link: 'test' },
-    },
-    d: {
-      player: 'found',
-    },
-    e: {
-      playing: { name: '/play test2', link: 'test2' },
-    },
-  }))
-})
-
-it('invalid command should call help', () => {
-  const event = createTestEvent({
-    content: '/invalid command',
-  })
-
-  const messages = handle(hot('a|', { a: event }))
-  expect(messages).toMatchObject(e('(ab)|', {
-    a: {
-      invalidCommand: '/invalid command',
-    },
-    b: {
-      help: true,
-    },
-  }))
-})
-
-it('disconnect should disconnect if connected to channel', () => {
-  const play = createTestEvent({
-    content: '/play test',
-  })
-  const disconnect = createTestEvent({
-    content: '/disconnect',
-  })
-
-  const messages = handle(hot('ab|', { a: play, b: disconnect }))
-  expect(messages).toMatchObject(e('(abc)(defg)|', {
-    a: {
-      commandCalled: '/play',
-    },
-    b: {
-      player: 'created',
-    },
-    c: {
-      playing: { name: '/play test', link: 'test' },
-    },
-    d: {
-      commandCalled: '/disconnect',
-    },
-    e: {
-      player: 'disconnected',
-    },
-    f: {
-      player: 'removed',
-    },
-    g: {
-      messageSent: 'Bye!',
-    },
-  }))
-})
-
-test('disconnect should not do anything if not connected to channel', () => {
-  const disconnect = createTestEvent({
-    content: '/disconnect',
-  })
-
-  const messages = handle(hot('a|', { a: disconnect }))
-  expect(messages).toMatchObject(e('a|', {
-    a: {
-      commandCalled: '/disconnect',
-    },
-  }))
 })
