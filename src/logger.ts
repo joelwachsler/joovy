2a62731a61505b5300a85d18fbffabd659babf1f
Joel <joel@wachsler.se>
2021-12-19 13:43:22 +0000

    Add timstamp to logger

--- src/logger.ts
+++ src/logger.ts
@@ -4,8 +4,9 @@ export default winston.createLogger({
   transports: [
     new winston.transports.Console({
       format: winston.format.combine(
+        winston.format.timestamp(),
         winston.format.colorize(),
-        winston.format.simple(),
+        winston.format.printf(info => `${info.timestamp} ${info.level} ${info.message}`),
       ),
     }),
   ],
