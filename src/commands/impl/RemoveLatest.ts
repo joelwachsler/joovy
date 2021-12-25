7aaa5dcd49f3b4f3d6860e8585d475f72a45218d
Joel <joel@wachsler.se>
2021-12-25 14:40:26 +0000

    Add remove command

--- src/commands/impl/RemoveLatest.ts
+++ src/commands/impl/RemoveLatest.ts
@@ -1,18 +1,22 @@
-import { concatMap, Observable } from 'rxjs'
+import { mergeMap, Observable } from 'rxjs'
 import JEvent from '../../jevent/JEvent'
 import { Result } from '../../jevent/Result'
 import { getPlaylist } from '../../playlist/Playlist'
 import ArgParser from '../ArgParser'
 import Command from '../command'
+import Remove from './Remove'
 
 export default class RemoveLatests implements Command {
   argument = ArgParser.create('removelatest')
   helpText = 'Removes the last added track.'
+  private remove = new Remove()
 
   handleMessage(event: JEvent): Observable<Result> {
     return getPlaylist(event).pipe(
-      concatMap(playlist => playlist.remove(playlist.queueLength - 1)),
-      concatMap(removedTrack => event.sendMessage(`${removedTrack.name} has been removed.`)),
+      mergeMap(playlist => {
+        const indexToRemove = playlist.queueLength - 1
+        return this.remove.remove(event, playlist, indexToRemove, indexToRemove)
+      }),
     )
   }
 }
