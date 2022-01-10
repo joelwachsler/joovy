d190aa4cbe7719eee518c0af2a2ad4107ff6d1cf
Joel Wachsler <joel@wachsler.se>
2022-01-10 16:23:03 +0000

    Filter empty result in logResult

--- core/src/resultLogger.ts
+++ core/src/resultLogger.ts
@@ -1,5 +1,5 @@
-import { Observable } from 'rxjs'
-import { Result } from './jevent/Result'
+import { filter, Observable } from 'rxjs'
+import { EmptyResult, Result } from './jevent/Result'
 import logger from './logger'
 
 /**
@@ -7,7 +7,9 @@ import logger from './logger'
  * Remember that you probably cannot reuse the observable passed to this function.
  */
 export const logResult = (handler: string, result: Observable<Result>) => {
-  return result.subscribe(({ result, event }) => {
-    logger.info(`${event.message.content} by ${event.message.author.id} (${event.message.author.username}) has been handled by ${handler} with result: ${result ? JSON.stringify(result) : result}`)
-  })
+  return result
+    .pipe(filter(r => !(r instanceof EmptyResult)))
+    .subscribe(({ result, event }) => {
+      logger.info(`${event.message.content} by ${event.message.author.id} (${event.message.author.username}) has been handled by ${handler} with result: ${result ? JSON.stringify(result) : result}`)
+    })
 }
